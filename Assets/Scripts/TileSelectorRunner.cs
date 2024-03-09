2925100cbf63ce77a907e485ac5804d4c8e43b90
David Napravnik <ebrithilnogare@gmail.com>
2024-03-09 16:58:09 +0100

    added guidlines

--- Assets/Scripts/TileSelectorRunner.cs
+++ Assets/Scripts/TileSelectorRunner.cs
@@ -13,11 +13,12 @@ public class TileSelectorRunner : MonoBehaviour
         Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraSize));
 
         int x = (int)System.Math.Round(worldMousePosition.x) + (int)(Constants.MAP_WIDTH / 2);
-        int y = -(int)System.Math.Round(worldMousePosition.y + .5);
+        int y = (int)System.Math.Round(worldMousePosition.y + .5);
+        Debug.Log("click: " + x + " " + y);
 
         if (y > 0 || x < 0 || x >= Constants.MAP_WIDTH)
-            selector.transform.position = new Vector3(-1, 0, 0);
+            selector.transform.localPosition = new Vector3(-1, 0, 0);
         else
-            selector.transform.position = new Vector3(x, y, 0);
+            selector.transform.localPosition = new Vector3(x, y, 0);
     }
 }
